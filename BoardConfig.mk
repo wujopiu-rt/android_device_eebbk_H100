1b414396fbabb3f5f5de049eeca2ed0a332a0532
XiKoTaSu_XKTS <3329989998@qq.com>
2025-12-20 17:21:22 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,3 +91,23 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# Language
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := zh_CN
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
+# System-as-root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Debug
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# defind version information
+TW_DEVICE_VERSION := $(shell date -u +"%Y%m%d"-"XKTS")
